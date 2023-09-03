38b45545a0af5a088950026b8d7a5d91a8474076
Mahmud-007 <mahmud6799@gmail.com>
2023-09-03 17:29:39 +0600

    add split screen

--- app/src/main/java/com/example/fconnecttest/MainActivity.java
+++ app/src/main/java/com/example/fconnecttest/MainActivity.java
@@ -1,22 +1,12 @@
 package com.example.fconnecttest;
 
-import static com.example.fconnecttest.R.id.bannerad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.ViewGroup;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.PopupWindow;
-import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,33 +15,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WebView myWebView = (WebView) findViewById(R.id.webview);
-        myWebView.loadUrl("https://sdk.footylight.com/eyecon");
-        myWebView.setMinimumHeight(220);
-
-        WebSettings webSettings = myWebView.getSettings();
+        WebView myWebView1 = (WebView) findViewById(R.id.webview1);
+        myWebView1.loadUrl("https://the-news-desk.com/displaytagVideo.html?p_key=HR2210JSY001&utm_medium=index&utm_source=HR2210JSY001");
+        myWebView1.setMinimumHeight(220);
+        WebSettings webSettings = myWebView1.getSettings();
         webSettings.setJavaScriptEnabled(true);
 
-        WebView bannerView = (WebView) findViewById(R.id.bannerad);
-        bannerView.loadUrl("https://sdk.footylight.com/bannerad");
-        bannerView.setMinimumHeight(220);
+        WebView myWebView = (WebView) findViewById(R.id.webview2);
+        myWebView.loadUrl("https://embed.footylight.com/fconnect-widget_v2/fl-av-player.html");
+//        myWebView.setMinimumHeight(220);
+        WebSettings webSettings2 = myWebView.getSettings();
+        webSettings2.setJavaScriptEnabled(true);
 
-        WebSettings bannerWebSettings = bannerView.getSettings();
-        bannerWebSettings.setJavaScriptEnabled(true);
-
-        Button btn = (Button) findViewById(R.id.popupbtn);
-        btn.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                openPopUpWidget();
-            }
-        });
     }
-
-    private void openPopUpWidget() {
-        Intent popupwidget = new Intent(MainActivity.this, PopUpWidget.class);
-        startActivity(popupwidget);
-    }
-
 }
