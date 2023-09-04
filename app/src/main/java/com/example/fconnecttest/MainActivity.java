2ff3b09b40c455a44073c1a650bb7d2b928fa988
Mahmud-007 <mahmud6799@gmail.com>
2023-09-04 12:34:30 +0600

    change app name & icon

--- app/src/main/java/com/example/fconnecttest/MainActivity.java
+++ app/src/main/java/com/example/fconnecttest/MainActivity.java
@@ -20,18 +20,12 @@ public class MainActivity extends AppCompatActivity {
         myWebView1.clearCache(true);
         myWebView1.getSettings().setDomStorageEnabled(true);
         myWebView1.loadUrl("https://the-news-desk.com/displaytagVideo.html?p_key=HR2210JSY001&utm_medium=index&utm_source=HR2210JSY001");
-//        myWebView1.setMinimumHeight(220);
-//        WebSettings webSettings = myWebView1.getSettings();
-//        webSettings.setJavaScriptEnabled(true);
 
         WebView myWebView2 = (WebView) findViewById(R.id.webview2);
         myWebView2.getSettings().setJavaScriptEnabled(true);
         myWebView2.clearCache(true);
         myWebView2.getSettings().setDomStorageEnabled(true);
         myWebView2.loadUrl("https://embed.footylight.com/fconnect-widget_v2/fl-av-player.html");
-//        myWebView2.setMinimumHeight(144);
-//        WebSettings webSettings2 = myWebView.getSettings();
-//        webSettings2.setJavaScriptEnabled(true);
 
     }
 }
